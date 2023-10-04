96c4d3e91439687f60bbc436eff51dba5505a9d8
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-04 13:22:43 +0100

    Implement menu option 3

--- RehomePets/Program.cs
+++ RehomePets/Program.cs
@@ -287,8 +287,66 @@ do
 
         case "3":
             // Ensure animal ages and physical descriptions are complete
-            Console.WriteLine("Challenge Project - please check back soon to see progress.");
-            Console.WriteLine("Press the Enter key to continue.");
+
+            // Check how many pets there are
+            petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
+
+            // Iterate through the pets
+            for (int i = 0; i < petCount; i++)
+            {
+                // Check pet ID is not default value
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    // Check age is default value
+                    if (ourAnimals[i, 2] == "Age: ?")
+                        {
+                            // If it is, ask the user to enter the age (display pet id)
+                            do
+                            {
+                                Console.WriteLine($"Enter an age for {ourAnimals[i, 0]}");
+                                readResult = Console.ReadLine();
+                                if (readResult != null)
+                                {
+                            // Check the entered age is a valid number
+                                    animalAge = readResult;
+                                    validEntry = int.TryParse(animalAge, out _);
+                                }
+                            } while (validEntry == false);
+                            // Update entry with pet age
+                            ourAnimals[i, 2] = $"Age: {animalAge}";
+                        }
+                    // Check if physical description is null or length 0
+                    if (ourAnimals[i, 4] == "Physical description: " || ourAnimals[i, 4] == "Physical description: tbd" || ourAnimals[i, 4].Length == 0 || ourAnimals[i, 4] == null)
+                    {
+                        // If so, ask the user to enter the description (display pet id)
+                        validEntry = false;
+                        do
+                        {
+                            Console.WriteLine($"Enter a physical description for {ourAnimals[i, 0]} (size, color, gender, weight, housebroken)");
+                            readResult = Console.ReadLine();
+                            if (readResult != null)
+                            {
+                                animalPhysicalDescription = readResult.ToLower();
+                                // Check the entered description is not empty
+                                if (animalPhysicalDescription.Length != 0)
+                                    validEntry = true;
+                            }
+                        } while (validEntry == false);
+                        // Update entry with pet age
+                        ourAnimals[i, 4] = $"Physical description: {animalPhysicalDescription}";
+                    }
+                }
+            }
+
+            Console.WriteLine("Age and physical description fields are complete for all of our friends.");
+            Console.WriteLine("Press the Enter key to continue");
             readResult = Console.ReadLine();
             break;
 
